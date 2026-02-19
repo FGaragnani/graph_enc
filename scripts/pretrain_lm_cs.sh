fa93a5dfa729137a944333a88900f4f296782e0a
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-02-19 11:09:33 +0100

    Update pretrain_lm_cs.sh

--- scripts/pretrain_lm_cs.sh
+++ scripts/pretrain_lm_cs.sh
@@ -5,7 +5,7 @@
 #SBATCH --open-mode=truncate
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=2
-#SBATCH --mem=40G
+#SBATCH --mem=80G
 #SBATCH --cpus-per-task=8
 #SBATCH --partition=all_usr_prod
 #SBATCH --account=ai4bio2025
