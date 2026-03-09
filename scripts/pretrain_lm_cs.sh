9ac111b2c0394e762b879c00973a3ba34ddfbec6
FGaragnani <filippogaragnani1@gmail.com>
2026-03-09 15:35:33 +0100

    Update pretrain_lm_cs.sh

--- scripts/pretrain_lm_cs.sh
+++ scripts/pretrain_lm_cs.sh
@@ -62,4 +62,5 @@ torchrun --nproc_per_node=2 --master_port=${MASTER_PORT} src/train/train.py \
   --save_steps 2000 \
   --evaluation_strategy epoch \
   --do_train \
-  --do_eval 
+  --do_eval \
+  --overwrite_output_dir
