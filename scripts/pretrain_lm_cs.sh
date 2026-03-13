ede85fa2fb1a0323100e915e318868cfac8a9ebb
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-03-13 17:29:56 +0100

    Update pretrain_lm_cs.sh

--- scripts/pretrain_lm_cs.sh
+++ scripts/pretrain_lm_cs.sh
@@ -51,8 +51,8 @@ torchrun --nproc_per_node=2 --master_port=${MASTER_PORT} src/train/train.py \
   --mlm_probability 0.15 \
   --item_length_proportion 1.0 \
   --output_dir ${output_dir} \
-  --per_device_train_batch_size 16 \
-  --per_device_eval_batch_size 16 \
+  --per_device_train_batch_size 8 \
+  --per_device_eval_batch_size 8 \
   --learning_rate 5e-4 \
   --max_steps 40000 \
   --warmup_steps 1200 \
