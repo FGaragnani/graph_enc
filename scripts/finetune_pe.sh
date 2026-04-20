a9eb69e70cc60e1d72fff2ed780636391630b86c
FGaragnani <filippogaragnani1@gmail.com>
2026-04-20 13:56:57 +0200

    Update finetune_pe.sh

--- scripts/finetune_pe.sh
+++ scripts/finetune_pe.sh
@@ -37,6 +37,7 @@ export MASTER_PORT=`comm -23 <(seq 5000 6000 | sort) <(ss -Htan | awk '{print $4
 torchrun --nproc_per_node=${SLURM_GPUS_PER_NODE} --master_port=${MASTER_PORT} src/task/discriminative.py \
   --model_name_or_path ${model_checkpoint} \
   --model_type bert \
+  --data_path /homes/fgaragnani/ai4bio/graph_enc/data/ \
   --tokenizer_name ./src/model \
   --config_file ./src/model/bert_config.json \
   --dataset_dir ${dataset_dir} \
