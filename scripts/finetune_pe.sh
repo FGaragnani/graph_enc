1969bed48f21d2fea265472128ffda33dbe5ca57
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-05-03 16:31:52 +0200

    Update finetune_pe.sh

--- scripts/finetune_pe.sh
+++ scripts/finetune_pe.sh
@@ -53,7 +53,7 @@ torchrun --nproc_per_node=${SLURM_GPUS_PER_NODE} --master_port=${MASTER_PORT} sr
   --max_chunks_per_sample 10 \
   --pad_to_max_length false \
   --output_dir ${run_output_dir} \
-  --per_device_train_batch_size 4 \
+  --per_device_train_batch_size 8 \
   --per_device_eval_batch_size 8 \
   --learning_rate 5e-5 \
   --max_steps 500 \
