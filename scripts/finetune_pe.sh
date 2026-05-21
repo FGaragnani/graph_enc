bf299d1b5da5cea4a473fa716e861f2187f98d22
FGaragnani <filippogaragnani1@gmail.com>
2026-05-21 11:15:34 +0200

    Update finetune_pe.sh

--- scripts/finetune_pe.sh
+++ scripts/finetune_pe.sh
@@ -9,6 +9,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --array=0-0
 #SBATCH --partition=all_usr_prod
+#SBATCH --constraint="a100|h100"
 #SBATCH --account=ai4bio2025
 #SBATCH --nodes=1
 #SBATCH --time=02:00:00
