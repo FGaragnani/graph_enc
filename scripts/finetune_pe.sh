18fb88e4a33033980dda0029bbf9c4c6b9df0c85
FGaragnani <filippogaragnani1@gmail.com>
2026-05-21 11:59:13 +0200

    Update finetune_pe.sh

--- scripts/finetune_pe.sh
+++ scripts/finetune_pe.sh
@@ -7,7 +7,7 @@
 #SBATCH --gpus-per-node=4
 #SBATCH --mem=180G
 #SBATCH --cpus-per-task=8
-#SBATCH --array=0-0
+#SBATCH --array=1-4
 #SBATCH --partition=all_usr_prod
 #SBATCH --constraint="gpu_A40_45G|gpu_L40S_45G|gpu_RTX6000_24G|gpu_RTX_A5000_24G"
 #SBATCH --account=ai4bio2025
